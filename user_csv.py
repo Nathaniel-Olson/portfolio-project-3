9cd165ec35305c2231524c705c0e1569581b96a1
Nathaniel Olson <nateolson07@gmail.com>
2025-11-29 14:52:10 -0700

    Blank Slate

---
+++ user_csv.py
@@ -0,0 +1,7 @@
+# user_csv.py
+# ENDG 233 F24
+# Nate Olson, Ruald James
+# Pairs L01 - 26
+# A terminal-based data analysis and visualization program in Python.
+# You must follow the specifications provided in the project description.
+# Remember to include docstrings and comments throughout your code.
